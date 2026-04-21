276889a03d21c85764aba7cead5c085d59e0eead
Hookibibi <115881751+Oukibibi@users.noreply.github.com>
2026-04-21 15:17:37 +0200

    setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home.js';
 import './App.css';
+import Stats from './Stats.js';
+import Streamers from './Streamers.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="#Stats">Stats</Link> |{" "}
+          <Link to="#Streamers">Streamers</Link>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="#Stats" element={<Stats />} />
+          <Route path="#Streamers" element={<Streamers />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
